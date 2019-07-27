4561c6c901fb0f096120d23f1b75d0263359ea36
Divyansh Prakash <divyansh.prakash@yahoo.com>
2019-07-28 00:09:07 +0530

    Allow custom handlers in navigable and scrollable lists

--- src/maggit/views.cljs
+++ src/maggit/views.cljs
@@ -3,6 +3,11 @@
             [maggit.core :refer [screen]]
             [maggit.keys :refer [with-keys]]))
 
+(defn- enhance-handler-map [handlers arg-atom]
+  (into {} (for [[keys f] handlers]
+             [keys #(f @arg-atom)])))
+
+
 (letfn [(cycle-next-item [curr items]
           (if (== (dec (count items)) curr)
             0
@@ -17,18 +22,20 @@
    - item-props: properties that will be applied to each item
    - selected: index of the currently selected item
    - on-select: function that will be called with the selected index when <right> is pressed
-   - on-back: function that will be called when <left> is pressed"
+   - on-back: function that will be called when <left> is pressed
+   - custom-key-handlers: {[\"left\" \"right\"] (fn [idx] (println idx))}"
     [{:keys [items item-props selected
-             on-select on-back]
+             on-select on-back custom-key-handlers]
       :or {on-select (fn [_])
            on-back (fn [])}
       :as props}]
     (r/with-let [selected (r/atom (or selected 0))]
       (with-keys @screen
-        {["down"]  #(swap! selected cycle-next-item items)
-         ["up"]    #(swap! selected cycle-prev-item items)
-         ["right"] #(on-select @selected)
-         ["left"]  on-back}
+        (merge {["down"]  #(swap! selected cycle-next-item items)
+                ["up"]    #(swap! selected cycle-prev-item items)
+                ["right"] #(on-select @selected)
+                ["left"]  on-back}
+               (enhance-handler-map custom-key-handlers selected))
         [:box (dissoc props
                       :items :item-props :selected
                       :on-select :on-back)
@@ -58,10 +65,11 @@
    - item-props: properties that will be applied to each item
    - selected: index of the currently selected item
    - on-select: function that will be called with the selected index when <right> is pressed
-   - on-back: function that will be called when <left> is pressed"
+   - on-back: function that will be called when <left> is pressed
+   - custom-key-handlers: {[\"left\" \"right\"] (fn [idx] (println idx))}"
     [{:keys [items item-props
              window-start window-size
-             on-select on-back]
+             on-select on-back custom-key-handlers]
       :or {on-select (fn [_])
            on-back (fn [])}
       :as props}]
@@ -69,16 +77,18 @@
                  window-size (or window-size 5)
                  window (r/atom (take window-size items))]
       (with-keys @screen
-        {["down"]  #(do (swap! window-start next-item items)
-                        (reset! window (get-window items
-                                                   @window-start
-                                                   window-size)))
-         ["up"]    #(do (swap! window-start prev-item items)
-                        (reset! window (get-window items
-                                                   @window-start
-                                                   window-size)))
-         ["right"] #(on-select @window-start)
-         ["left"]  on-back}
+        (merge
+         {["down"]  #(do (swap! window-start next-item items)
+                         (reset! window (get-window items
+                                                    @window-start
+                                                    window-size)))
+          ["up"]    #(do (swap! window-start prev-item items)
+                         (reset! window (get-window items
+                                                    @window-start
+                                                    window-size)))
+          ["right"] #(on-select @window-start)
+          ["left"]  on-back}
+         (enhance-handler-map custom-key-handlers window-start))
         [:box (dissoc props
                       :items :item-props :selected
                       :on-select :on-back)
