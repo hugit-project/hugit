fbda8932568fbad67630d500fac9da46f6e55051
Divyansh Prakash <divyansh.prakash@yahoo.com>
2019-08-03 02:12:23 +0530

    maggit.shell/exec-promise

--- src/maggit/shell.cljs
+++ src/maggit/shell.cljs
@@ -3,7 +3,37 @@
 (defonce shelljs
   (js/require "shelljs"))
 
-(defn exec [& strings]
-  (.exec shelljs
-         (clojure.string/join strings)
-         #js {:silent true}))
+(defn exec*
+  [command & {:keys [async silent encoding callback]
+              :or {async false
+                   silent false
+                   encoding "utf8"
+                   callback (fn [_])}
+              :as options}]
+  (let [opts (dissoc options :callback)
+        res (.exec shelljs
+                   command
+                   (clj->js options)
+                   callback)]
+    {:code (.-code res)
+     :stdout (.-stdout res)
+     :stderr (.-stderr res)}))
+
+(defn exec
+  [& strings]
+  (exec*  (str/join strings)
+          :silent true))
+
+(defn exec-promise
+  [& strings]
+  (js/Promise.
+   (fn [resolve]
+     (letfn [(callback [code stdout stderr]
+               (println stdout)
+               (resolve {:code code
+                         :stdout stdout
+                         :stderr stderr}))]
+       (exec* (str/join strings)
+              :silent true
+              :async true
+              :callback callback)))))
