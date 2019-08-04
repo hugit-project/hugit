25737e1892c1201dbbeb23640217820a83b8b47f
Divyansh Prakash <divyansh.prakash@yahoo.com>
2019-08-05 01:45:14 +0530

    Unify dev and release code

--- src/maggit/logs.cljs
+++ src/maggit/logs.cljs
@@ -33,6 +33,14 @@
  (fn [db _]
    (assoc db :logs/show-logs? false)))
 
+(set! (.-log js/console) log)
+(set! (.-error js/console) log)
+(set! (.-info js/console) log)
+(set! (.-debug js/console) log)
+(re-frame.loggers/set-loggers! {:log log
+                                :error log
+                                :warn log})
+
 (defn log-box
   "Display a box that shows the last several lines of logged output based on
   screen height.
