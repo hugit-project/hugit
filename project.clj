2f138740f3ef3612051f764b6f5d124ef57f02ec
Divyansh Prakash <divyansh.prakash@yahoo.com>
2019-08-19 14:47:50 +0530

    lein editor

--- project.clj
+++ project.clj
@@ -1,3 +1,6 @@
 (defproject hugit "0.1.0"
+  :min-lein-version "2.0.0"
   :dependencies [[thheller/shadow-cljs "2.8.50"]]
-  :source-paths ["src" "dev"])
+  :plugins [[nightlight/lein-nightlight "RELEASE"]]
+  :source-paths ["src" "dev"]
+  :aliases {"editor" ["nightlight" "--url" "\"http://localhost:$(cat .shadow-cljs/socket-repl.port)\""]})
